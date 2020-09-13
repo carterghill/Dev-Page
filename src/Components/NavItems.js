5579dbdc4504135fadcad4893496877b3dece51c
unknown <carterghill@gmail.com>
2020-09-13 03:44:20 -0600

    added menu items

---
+++ src/Components/NavItems.js
@@ -0,0 +1,18 @@
+export const NavItems = [
+    {
+        title: 'Home',
+        url: '#'
+    },
+    {
+        title: 'Projects',
+        url: '#'
+    },
+    {
+        title: 'About',
+        url: '#'
+    },
+    {
+        title: 'Resources',
+        url: '#'
+    }
+]
