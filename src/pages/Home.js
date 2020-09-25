8d4affe0ea3c52b4a427122b0e77efafee311746
carterghill <carterghill@gmail.com>
2020-09-25 13:18:03 -0600

    Added page files after moving them

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Slogan from '../Components/Slogan'
+import '../App.css';
+
+function App() {
+  return (
+    <div>
+        <div className='header'>Solo Web Design and App Developement</div>
+
+        <Slogan title='Custom Tailored Code'
+                info="I will make sure that your custom app or website is suited for your needs" />
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/pages/Resources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+function App() {
+  return (
+    <div>
+        <h2>Resources</h2>
+    </div>
+
+  );
+}
+
+export default App;
