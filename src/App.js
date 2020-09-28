6e5e19a2d7b2af54e2249dc625ddc2f0228aaa7a
carterghill <carterghill@gmail.com>
2020-09-28 13:00:54 -0600

    Added page fade-in and fade-out for smoother page transition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from "./Components/Navbar"
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import Home from "./Pages/Home"
 import Projects from "./Pages/Projects"
 import About from "./Pages/About"
@@ -23,15 +24,26 @@ function App() {
             <Navbar />
 
             <div className='page'>
-                <Switch>
-                    <Route path="/" component={Home} exact={true}/>
-                    <Route path="/home" component={Home}/>
-                    <Route path="/projects" component={Projects}/>
-                    <Route path="/about" component={About}/>
-                    <Route path="/resources" component={Resources}/>
-                    <Route path="/contact" component={Contact}/>
-                    <Route component={PageNotFound} exact={true}/>
-                </Switch>
+                <Route render={({location}) => (
+                    <TransitionGroup>
+                        <CSSTransition
+                            key={location.key}
+                            timeout={333}
+                            classNames='fade-route'
+                        >
+                        <Switch location={location}>
+                            <Route id='route' path="/" component={Home} exact={true}/>
+                            <Route id='route' path="/home" component={Home} exact={true}/>
+                            <Route id='route' path="/projects" component={Projects} exact={true}/>
+                            <Route id='route' path="/about" component={About} exact={true}/>
+                            <Route id='route' path="/resources" component={Resources} exact={true}/>
+                            <Route id='route' path="/contact" component={Contact} />
+                            <Route id='route' component={PageNotFound} exact={true}/>
+                        </Switch>
+                        </CSSTransition>
+                    </TransitionGroup>
+                )} />
+
             </div>
 
         </BrowserRouter>
